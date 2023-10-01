9ef7f7f86d73f4d997b59bad1ad3791768e46f5d
Gabe Klein <gabe@termtm.com>
2023-10-01 16:07:28 -0400

    separate invalidation for css and js

--- plugin.ts
+++ plugin.ts
@@ -1,12 +1,13 @@
-import babel from '@babel/core';
-import { Options as TransformOptions } from '@expressive/babel-preset-react';
+import { Options as TransformOptions } from "@expressive/babel-preset-react"
+import * as babel from '@babel/core';
 import path from 'path';
-import { Plugin } from 'rollup';
-import { ViteDevServer } from 'vite';
+import fs from 'fs/promises';
+import { Plugin } from "rollup";
+import { ModuleNode, ViteDevServer } from "vite";
 
 const CWD = process.cwd();
 const PREFIX = "\0virtual:";
-const relative = path.relative.bind(null, CWD);
+const relative = path.relative.bind(path, CWD);
 
 /**
  * Rollup plugin but also may be used in
@@ -22,7 +23,7 @@ interface PluginCompat extends Plugin {
 }
 
 export interface Options extends TransformOptions {
-  test?: RegExp | ((uri: string, code: string) => boolean);
+  test?: RegExp | ((uri: string) => boolean);
 }
 
 function jsxPlugin(options?: Options): PluginCompat {
@@ -42,7 +43,7 @@ function jsxPlugin(options?: Options): PluginCompat {
    * In the event a vite development server is running, we
    * can use it to reload the module when the CSS changes.
    */
-  let server: ViteDevServer
+  let server: ViteDevServer;
 
   return {
     name: "expressive-jsx",
@@ -54,33 +55,56 @@ function jsxPlugin(options?: Options): PluginCompat {
       if(id === "__EXPRESSIVE_CSS__")
         return '\0virtual:' + relative(importer) + ".css";
     },
-    load(id: string) {
-      if(id.startsWith(PREFIX))
-        return CACHE.get(id.slice(9, -4))!.css;
-    },
-    async transform(source, id){
-      if(/node_modules/.test(id) || !shouldTransform(id, source))
+    async load(path: string) {
+      if(path.startsWith(PREFIX))
+        return CACHE.get(path.slice(9, -4))!.css;
+
+      if(/node_modules/.test(path))
         return;
 
-      id = relative(id);
+      const id = relative(path);
 
-      const cached = CACHE.get(id);
+      if(CACHE.has(id))
+        return CACHE.get(id);
+
+      const source = await fs.readFile(path, "utf8");
+
+      if(!shouldTransform(id))
+        return;
+
       const result = await transform(id, source, options);
 
-      if(server){
-        const { css } = result;
-
-        if(cached && css !== cached.css){
-          const module = server.moduleGraph.getModuleById("\0virtual:" + id + ".css");
-  
-          if(module)
-            server.reloadModule(module);
-        }
+      if(result.css){
+        CACHE.set(id, result);
+        return result.code;
       }
-  
+    },
+    async handleHotUpdate(context){
+      const { file } = context;
+      const id = relative(file);
+      const cached = CACHE.get(id);
+
+      if(!cached)
+        return;
+
+      const { moduleGraph } = server;
+      const source = await context.read();
+      const result = await transform(file, source, options);
+      const invalidate: ModuleNode[] = [];
+
       CACHE.set(id, result);
 
-      return result;
+      if(cached.code !== result.code){
+        const module = moduleGraph.getModuleById(file);
+        invalidate.push(module!);
+      }
+
+      if(cached.css !== result.css){
+        const module = moduleGraph.getModuleById("\0virtual:" + id + ".css");
+        invalidate.push(module!);
+      }
+
+      return invalidate;
     }
   }
 }
@@ -124,7 +148,10 @@ async function transform(id: string, input: string, options?: Options){
   if(!result)
     throw new Error("No result");
 
-  const code = result.code + `\nimport "__EXPRESSIVE_CSS__";`;
+  let { code } = result;
+
+  if(css)
+    code += `\nimport "__EXPRESSIVE_CSS__";`;
 
   return <TransformResult> {
     code,
